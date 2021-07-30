c0c4ce899955c4740583eaec499dff42cb43fc57
Phuc <gavotte.tran@gmail.com>
2021-07-30 23:14:56 +0700

    done item layouts for recyclerView

--- app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoFragment.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoFragment.kt
@@ -2,17 +2,18 @@ package com.example.android.politicalpreparedness.election
 
 import android.os.Bundle
 import android.view.*
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.example.android.politicalpreparedness.R
+import com.example.android.politicalpreparedness.databinding.FragmentVoterInfoBinding
 
 class VoterInfoFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-
+        val binding = DataBindingUtil.inflate<FragmentVoterInfoBinding>(inflater, R.layout.fragment_voter_info, container, false)
         //TODO: Add ViewModel values and create ViewModel
-
-        //TODO: Add binding values
 
         //TODO: Populate voter info -- hide views without provided data.
         /**
@@ -24,7 +25,7 @@ class VoterInfoFragment : Fragment() {
 
         //TODO: Handle save button UI state
         //TODO: cont'd Handle save button clicks
-
+        return binding.root
     }
 
     //TODO: Create method to load URL intents
